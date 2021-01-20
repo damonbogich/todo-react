436d14a15ea4e2c72e97da386d664d8f6cf77628
Damon Bogich <damonbogich@gmail.com>
2021-01-20 12:43:49 -0700

    initial state data displaying

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import React from 'react';
+import TodoList from './components/TodoList';
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      tasks: [{
+        task: 'Meditation',
+        id: 1528817077286,
+        completed: false
+      },
+      {
+        task: 'Shop for boots',
+        id: 1528817084358,
+        completed: false
+      },
+      {
+        task: 'watch warriors game',
+        id: 1528817084368,
+        completed: false
+      }
+    ]
+    }
+  }
+
+
+
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoList tasks={this.state.tasks} />
       </div>
     );
   }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,19 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+import Todo from './Todo';
+
+export default function TodoList ({tasks}) {
+    console.log(tasks, 'props from list')
+    return (
+        <div>
+            to do list component where todos will be generated
+            <ul>
+            {tasks.map((task) => {
+                return <Todo key={task.id} task={task.task}/>
+            })}
+            </ul>
+            
+        </div>
+    )
+}
